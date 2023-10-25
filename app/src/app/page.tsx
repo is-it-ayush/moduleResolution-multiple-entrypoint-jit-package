a0c5c4935dc6f18d544989525f35ac1c3a747de2
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-10-25 16:00:35 +0530

    chore: slight fixy

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,14 +1,11 @@
 
-// module resolution: bundler
-// import { x_a, type A  } from "@vendor/package/x/a"; // directory export breaks.
-// import { y } from "@vendor/package/y"; // individual file export works
-
-// moduleResolution: node / nodenext
-import { x_a, type A  } from "@vendor/package/x/a"; // directory export breaks.
-import { y } from "@vendor/package/y"; // individual file export works
+import { x_a, type A } from "@vendor/package/x/a";
+import { y } from "@vendor/package/y";
 
 export default async function Home() {
+  type HoverMe = A;
   x_a();
+  y();
   return (
     <main className="flex min-h-screen bg-black text-white">
     </main>
